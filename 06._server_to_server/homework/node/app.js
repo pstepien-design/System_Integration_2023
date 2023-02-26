904d574af49c335115348b5828a0227b6bfd27af
Paweł Stępień <pstepien@interia.pl>
2023-02-26 14:47:52 +0100

    added txt and csv readers for python and node

--- 06._server_to_server/homework/node/app.js
+++ 06._server_to_server/homework/node/app.js
@@ -4,6 +4,7 @@ import Papa from "papaparse";
 
 const app = express();
 
+// CSV
 app.get("/getCsvFromNode", (req, res) => {
   fs.readFile("../data/me.csv", "utf-8", (err, data) => {
     if (err) {
@@ -17,6 +18,7 @@ app.get("/getCsvFromNode", (req, res) => {
         myObject[key] = parsedCsv.data[1][index];
       });
 
+      console.log(parsedCsv.data[1]);
       res.send(myObject);
     }
   });
@@ -24,6 +26,55 @@ app.get("/getCsvFromNode", (req, res) => {
 
 app.get("/getCsvFromPython", (req, res) => {
   fetch("http://127.0.0.1:8000/getCsvFromPython")
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+});
+
+// JSON
+app.get("/getJsonFromNode", (req, res) => {
+  fs.readFile("../data/me.json", "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const jsonData = JSON.parse(data);
+      const meObject = {
+        ...jsonData,
+      };
+
+      res.send(meObject);
+    }
+  });
+});
+
+app.get("/getJsonFromPython", (req, res) => {
+  fetch("http://127.0.0.1:8000/getJsonFromPython")
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+});
+
+// TXT
+app.get("/getTxtFromNode", (req, res) => {
+  fs.readFile("../data/me.txt", "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const [name, country, languages] = data
+        .split("\n")
+        .map((line) => line.trim());
+
+      const meObject = {
+        name,
+        country,
+        languages: languages.split(", "),
+      };
+
+      res.send(meObject);
+    }
+  });
+});
+
+app.get("/getTxtFromPython", (req, res) => {
+  fetch("http://127.0.0.1:8000/getTxtFromPython")
     .then((response) => response.json())
     .then((data) => res.send(data));
 });
--- 06._server_to_server/homework/python/main.py
+++ 06._server_to_server/homework/python/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from datetime import datetime
 import requests
 import csv
+import json
 
 app = FastAPI()
 
+
+# CSB
 @app.get("/getCsvFromPython")
-def get_date():
+def get_csv_from_python():
     with open("../data/me.csv") as file:
 
         data = csv.DictReader(file)
@@ -23,8 +26,52 @@ def get_date():
     return meObject
 
 @app.get("/getCsvFromNode")
-def get_date_from_express():
+def get_csv_from_node():
     response = requests.get("http://127.0.0.1:8080/getCsvFromNode")
     date = response.json()
     return date
+
+# JSON
+
+@app.get("/getJsonFromPython")
+def get_json_from_python():
+    with open("../data/me.json") as file:
+
+        data = json.load(file)
+
+        meObject = data
+
+        return meObject
+
+@app.get("/getJsonFromNode")
+def get_json_from_node():
+    response = requests.get("http://127.0.0.1:8080/getJsonFromNode")
+    date = response.json()
+    return date
+
+# TXT
+
+@app.get("/getTxtFromPython")
+def get_txt_from_python():
+    with open("../data/me.txt") as file:
+
+        data = file.read()
+
+        name, country, languages = data.strip().split("\n")
+
+        meObject = {
+            "name": name,
+            "country": country,
+            "languages": languages.split(", ")
+        }
+
+        return meObject
+
+@app.get("/getTxtFromNode")
+def get_txt_from_node():
+    response = requests.get("http://127.0.0.1:8080/getTxtFromNode")
+    date = response.json()
+    return date
+
+
 
