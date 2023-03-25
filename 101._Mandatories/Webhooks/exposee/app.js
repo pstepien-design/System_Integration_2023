12f7b4fb6562e4cba0e55117852342f007789cde
Paweł Stępień <pstepien@interia.pl>
2023-03-25 17:12:21 +0100

    added the swagger for the webhooks endpoints

--- 101._Mandatories/Webhooks/exposee/app.js
+++ 101._Mandatories/Webhooks/exposee/app.js
@@ -9,5 +9,22 @@ app.use(express.json());
 app.use(webhooksRouter);
 app.use(paymentRouter);
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "OpenAPI API for the Webhooks",
+    version: "1.0.0",
+    description: "A webhooks Express API",
+  },
+};
+const options = {
+  swaggerDefinition,
+  apis: ["./routers/webhooks.js"],
+};
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
+
 const PORT = 8080;
 app.listen(PORT, () => console.log("Exposer server is running on port", PORT));
--- 101._Mandatories/Webhooks/exposee/routers/webhooks.js
+++ 101._Mandatories/Webhooks/exposee/routers/webhooks.js
@@ -8,19 +8,97 @@ import {
 
 const router = new Router();
 
+/**
+ * @swagger
+ * /webhook/register:
+ *   post:
+ *     summary: Register a webhook URL
+ *     description: Use this endpoint to register your url, so it is called whenever the payment is completed or processed
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               urlToRegister:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Webhook registration succeeded
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Success message
+ *       500:
+ *         description: Webhook registration failed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 router.post("/webhook/register", (req, res) => {
   const url = req.body.urlToRegister;
-  addUrlToFile(url)
-    .then(() =>
-      res.status(200).send({
-        message: `Webhook registration succeeded for url: ${url}`,
-      })
-    )
-    .catch(() =>
-      res.status(500).send({ message: "Webhook registration failed" })
-    );
+  if (url) {
+    addUrlToFile(url)
+      .then(() =>
+        res.status(200).send({
+          message: `Webhook registration succeeded for url: ${url}`,
+        })
+      )
+      .catch(() =>
+        res.status(500).send({ message: "Webhook registration failed" })
+      );
+  } else {
+    res.status(500).send({ message: "Webhook registration failed" });
+  }
 });
 
+/**
+ * @swagger
+ * /webhook/unregister:
+ *   post:
+ *     summary: Unregister a webhook URL
+ *     description: Use this endpoint to unregister your url. The url will not be called if the payment is completed or processed
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               urlToRegister:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Webhook has been successfully unregistered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Success message
+ *       500:
+ *         description: Webhook could not be unregistered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 router.post("/webhook/unregister", (req, res) => {
   const url = req.body.urlToRegister;
   if (url) {
@@ -36,6 +114,39 @@ router.post("/webhook/unregister", (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /webhook/ping:
+ *   post:
+ *     summary: Ping a webhook URL
+ *     description: Use this endpoint to test whether your url is registered or not. If it is registered, the url will be called with a message. If not, you will receive an error message.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               urlToRegister:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Webhook is registered for payment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       400:
+ *         description: URL is not registered for webhooks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 router.post("/webhook/ping", async (req, res) => {
   const allRegisteredUrls = await getAllRegisteredUrls();
   const requestedUrl = req.body.urlToRegister;
@@ -51,7 +162,7 @@ router.post("/webhook/ping", async (req, res) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        message: `${requestedUrl} is registered for payment`,
+        message: `${requestedUrl} is registered for payment webhooks`,
       }),
     });
     res.send({});
