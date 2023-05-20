a550ca248f9f9298640385372645ed3b0e3a7bfc
Paweł Stępień <pstepien@interia.pl>
2023-05-20 18:51:53 +0200

    feat: made the web scarping save the news to the file

--- 101._Mandatories/Web_scraping_Mandatory_2_Part_2/index.js
+++ 101._Mandatories/Web_scraping_Mandatory_2_Part_2/index.js
@@ -1,15 +1,14 @@
 import { load } from "cheerio";
+import dayjs from "dayjs";
 import fs from "fs";
 
 const response = await fetch("https://nyheder.tv2.dk/");
 const result = await response.text();
 
-let page = fs.readFileSync("index.html", "utf-8");
+fs.writeFileSync("index.html", "");
 
-if (!page.includes("html")) {
-  fs.writeFileSync("index.html", result);
-  page = fs.readFileSync("index.html", "utf-8");
-}
+fs.writeFileSync("index.html", result);
+const page = fs.readFileSync("index.html", "utf-8");
 
 const $ = load(page);
 
@@ -22,5 +21,19 @@ $(".tc_teaser__header")
 
     titles.push(data);
   });
+
+const date = dayjs().format("DD-MM-YYYY_HH-mm-ss");
+
+const fileName = `./${date}-nyheder.txt`;
+
+fs.writeFile(
+  `./news/${fileName}`,
+  titles.join("\n"),
+  { flags: "a" },
+  function (err) {
+    if (err) throw err;
+    console.log("Newest news for ", date, " saved");
+  }
+);
 
 console.log("Nyheder", titles);
